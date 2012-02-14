bdccc2fef6e2b6f7f5ae5e4f7e745a7247d75189
Marek Majkowski <marek@rabbitmq.com>
2012-02-14 15:24:13 +0000

    Don't depend on cowboy, use dev branch, include all licenses

--- package.mk
+++ package.mk
@@ -1,14 +1,12 @@
-DEPS:=cowboy-wrapper
-
 APP_NAME:=sockjs
 
 UPSTREAM_GIT:=https://github.com/sockjs/sockjs-erlang.git
-UPSTREAM_REVISION:=5db8e68400d74aad52c1257311591e837611a492
+UPSTREAM_REVISION:=dev
 RETAIN_ORIGINAL_VERSION:=true
 
 ORIGINAL_APP_FILE:=$(CLONE_DIR)/src/$(APP_NAME).app.src
 DO_NOT_GENERATE_APP_FILE=true
 
 define construct_app_commands
-	cp $(CLONE_DIR)/LICENSE-MIT-SockJS $(APP_DIR)
+	cp $(CLONE_DIR)/LICENSE-* $(APP_DIR)
 endef
