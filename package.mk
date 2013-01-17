a84801239f7f4572ecd4a8695e315fa21161bfbc
Simon MacMullen <simon@rabbitmq.com>
2013-01-17 18:09:40 +0000

    Update to 0.3.4.

--- package.mk
+++ package.mk
@@ -2,10 +2,11 @@ APP_NAME:=sockjs
 DEPS:=cowboy-wrapper
 
 UPSTREAM_GIT:=https://github.com/rabbitmq/sockjs-erlang.git
-UPSTREAM_REVISION:=92d4ba4e41f6e110f97cb50ea0810e34e9b8ef4f
+UPSTREAM_REVISION:=3132eb920aea9abd5c5e65349331c32d8cfa961e # 0.3.4
 RETAIN_ORIGINAL_VERSION:=true
 WRAPPER_PATCHES:=\
-	0000-remove-spec-patch.diff
+        0000-remove-spec-patch.diff \
+        0001-a2b-b2a.diff
 
 ORIGINAL_APP_FILE:=$(CLONE_DIR)/src/$(APP_NAME).app.src
 DO_NOT_GENERATE_APP_FILE=true
